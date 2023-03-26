b6de28894a0b93ba1010866c54aa948c66b12585
Megan Buck <thequiltingriverotter@gmail.com>
2023-03-25 14:29:38 -1000

    Fixed bug with mouseenter triggering twice. Added greyscale function. Added border toggle function. Changed style for better appearance

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const grid = document.querySelector("#grid");
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".buttons");
 const slider = document.querySelector("#gridSize");
-let borderDisplay = 'solid black 1px';
+let isBordersOn = false;
 
 let gridSize = 16;
 
@@ -10,8 +10,9 @@ slider.oninput = function () {
     gridSize = this.value;
     clearGrid();
     displayGrid(gridSize);
+    document.querySelector("#slideDisplay").innerHTML = `${gridSize} X ${gridSize}`;
     cells = document.querySelectorAll(".cell");
-    changeCellColor();
+    changeColor();
 }
 
 
@@ -23,7 +24,7 @@ function displayGrid(size = 16) {
         for (let j = 0; j < size; j++) {
             let cell = document.createElement("div");
             cell.classList.add("cell");
-            cell.style.border = borderDisplay;
+            cell.style.border = "none";
             row.append(cell);
         }
         grid.append(row);
@@ -38,24 +39,48 @@ function clearGrid() {
     })
 }
 
-
-function changeCellColor(color = 'black') {
+function changeColor() {
     cells.forEach(cell => {
         cell.addEventListener("mouseenter", (e) => {
-            console.log('color');
-            if (color === 'rainbow') {
+            if (isBlackRunning) {
+                e.target.style.backgroundColor = "rgb(0, 0, 0)";
+            } else if (isColorRunning) {
                 e.target.style.backgroundColor = getRandomColor();
+            } else if (isGreyScaleRunning) {
+                e.target.style.backgroundColor = getGreyScale(e.target);
             }
-            else if (color === 'greyScale') {
-                e.target.style.backgroundColor = 'rgb(240, 240, 240)';
-
-            }
-            else {
-                e.target.style.backgroundColor = color;
-            }
-
         })
+
     })
+
+}
+
+function getIsColor(currentColor, colorCode) {
+    const firstIndex = Number(currentColor.indexOf(",")) + 2;
+    const secondIndex = Number(colorCode.length) + firstIndex;
+    const secondCode = currentColor.slice(firstIndex, secondIndex);
+    if (colorCode != secondCode) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function getGreyScale(element) {
+    const currentColor = window.getComputedStyle(element).backgroundColor;
+    const firstIndex = Number(currentColor.indexOf("(")) + 1;
+    const secondIndex = Number(currentColor.indexOf(","));
+    const colorCode = currentColor.slice(firstIndex, secondIndex);
+    if (getIsColor(currentColor, colorCode) || colorCode == 0 || colorCode == 255) {
+        return "rgb(220, 220, 220)";
+    } else {
+        let value = colorCode - 20;
+        if (value <= 0) {
+            return "rgb(1,1,1)";
+        }
+        return `rgb(${value}, ${value}, ${value})`;
+    }
 }
 
 function getRandomColor() {
@@ -68,25 +93,41 @@ function getRandomColor() {
 
 displayGrid(gridSize);
 let cells = document.querySelectorAll(".cell");
-changeCellColor('black');
+let isBlackRunning = true;
+let isColorRunning = false;
+let isGreyScaleRunning = false;
+
+changeColor();
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         if (e.target.id === "rainbow") {
-            changeCellColor("rainbow");
+            isBlackRunning = false;
+            isGreyScaleRunning = false;
+            isColorRunning = true;
         } else if (e.target.id === "black") {
-            changeCellColor("black");
+            isBlackRunning = true;
+            isColorRunning = false;
+            isGreyScaleRunning = false;
         } else if (e.target.id === "greyScale") {
-            changeCellColor('greyScale');
+            isGreyScaleRunning = true;
+            isColorRunning = false;
+            isBlackRunning = false;
         } else if (e.target.id === "reset") {
             cells.forEach(cell => {
                 cell.style.backgroundColor = "white";
             })
         } else if (e.target.id === 'borders') {
             cells.forEach(cell => {
-                cell.style.border = 'none';
+                if (isBordersOn) {
+                    cell.style.border = "none";
+                } else {
+                    cell.style.border = "1px solid black";
+                }
+
             })
-            borderDisplay = 'none';
+            isBordersOn = (isBordersOn) ? false : true;
+
         }
 
     })
